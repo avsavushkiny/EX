83412d9af8c0012f6ba81a3e64ed3d0a1c67dafa
Alexander Savushkin <74372739+avsavushkiny@users.noreply.github.com>
2026-07-31 11:31:35 +0300

    73126
    new style OTA web page, bug fix

--- src/ota2/OTAWebUpdater.h
+++ src/ota2/OTAWebUpdater.h
@@ -7,42 +7,43 @@
 #include <ESPmDNS.h>
 #include <Update.h>
 
-class OTAWebUpdater {
+class OTAWebUpdater
+{
 public:
     OTAWebUpdater();
     ~OTAWebUpdater();
 
     // Инициализация как WiFi точка доступа
-    bool begin(const char* apSSID = "EX-OTA", const char* apPassword = "");
-    
+    bool begin(const char *apSSID = "ESP32-OTA", const char *apPassword = "");
+
     // Запуск веб-сервера
     void startWebServer(int port = 80);
-    
+
     // Обработка клиентов (должна вызываться в loop)
     void handleClient();
-    
+
     // Получение статуса
     String getLastStatus() const;
-    
+
     // Получение IP адреса
     IPAddress getLocalIP() const;
 
 private:
-    WebServer* _server;
+    WebServer *_server;
     String _apSSID;
     String _lastStatus;
     IPAddress _localIP;
-    
+
     // HTML страница в стиле Microsoft
     String getUpdatePageHTML();
-    
+
     // Обработчики веб-запросов
     void handleRoot();
     void handleUpdate();
     void handleFirmwareUpload();
     void handleStatus();
     void handleNotFound();
-    
+
     // Вспомогательные функции
     String getBytes(uint64_t bytes);
 };
